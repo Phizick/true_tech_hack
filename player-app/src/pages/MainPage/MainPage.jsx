22a55ef9cfbba38bead535e283f7fb9d93ca9266
Nick White <bbycinka@yandex.ru>
2023-03-25 19:03:26 +0300

    feat:(frontend): player react-player

--- player-app/src/pages/MainPage/MainPage.jsx
+++ player-app/src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,16 @@
 import { useDispatch } from "react-redux";
 import stylesMainPage from "./MainPage.module.css";
 import { testAction } from "../../service/slices/test";
+import {
+  Player,
+  ControlBar,
+  ReplayControl,
+  ForwardControl,
+  CurrentTimeDisplay,
+  TimeDivider,
+  PlaybackRateMenuButton,
+  VolumeMenuButton,
+} from "video-react";
 export const MainPage = () => {
   const dispatch = useDispatch();
   const testRedux = () => {
@@ -10,6 +20,19 @@ export const MainPage = () => {
     <div>
       <h1>MainPage</h1>
       <button onClick={testRedux}>test Redux</button>
+      <Player poster="/assets/poster.png">
+        <source src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4" />
+        <source src="http://mirrorblender.top-ix.org/movies/sintel-1024-surround.mp4" />
+
+        <ControlBar>
+          <ReplayControl seconds={10} order={1.1} />
+          <ForwardControl seconds={30} order={1.2} />
+          <CurrentTimeDisplay order={4.1} />
+          <TimeDivider order={4.2} />
+          <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+          <VolumeMenuButton disabled />
+        </ControlBar>
+      </Player>
     </div>
   );
 };
